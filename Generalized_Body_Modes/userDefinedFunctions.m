f3a6053f4a3eee4687a5dccc53ea03de63e2d032
akeeste <akeeste@sandia.gov>
2021-01-29 15:18:55 -0600

    update GBM post-processing to highlight the GBM dofs

--- Generalized_Body_Modes/userDefinedFunctions.m
+++ Generalized_Body_Modes/userDefinedFunctions.m
@@ -1,15 +1,9 @@
-imcr=1;
-mcr.maxPitch(imcr) = sin(max(output.bodies.position(round(end/2):end,5)))*40;
-mcr.maxHeave(imcr) = max(output.bodies.position(:,3));
-mcr.maxSurge(imcr) = max(output.bodies.position(:,1));
-mcr.waveT(imcr) = waves.T;
-
 %Example of user input MATLAB file for post processing
 
 %Plot waves
 waves.plotEta(simu.rampTime);
 try 
-    waves.plotSpectrum();
+waves.plotSpectrum();
 catch
 end
 
@@ -19,6 +13,35 @@ output.plotResponse(1,1);
 %Plot heave response for body 1
 output.plotResponse(1,3);
 
-
 %Plot heave forces for body 1
 output.plotForces(1,3);
+
+% GBM response
+% Flex_out.signals.values(t/dt,40): 
+% pos x 4, vel x 4, acc x 4, ftotal x 4, fex x 4, 
+% frd x 4, fam x 4, fhs x 4, fdrag  x 4, fld x 4,
+for i=1:4
+    figure()
+    plot(Flex_out.time, Flex_out.signals.values(:,i),'r',...
+        Flex_out.time, Flex_out.signals.values(:,4+i),'b',...
+        Flex_out.time, Flex_out.signals.values(:,8+i),'k');
+    legend({'position','velocity','acceleration'});
+    xlabel('Time (s)');
+    ylabel('Response in (m) or (radians)');
+    title(['body1(barge) GBM dof ' int2str(i) ' Response']);
+end
+
+figure()
+i = 1;
+plot(Flex_out.time, Flex_out.signals.values(:,12+i),...
+    Flex_out.time, Flex_out.signals.values(:,16+i),...
+    Flex_out.time, -Flex_out.signals.values(:,20+i),...
+    Flex_out.time, -Flex_out.signals.values(:,24+i),...
+    Flex_out.time, -Flex_out.signals.values(:,28+i),...
+    Flex_out.time, -Flex_out.signals.values(:,32+i),...
+    Flex_out.time, -Flex_out.signals.values(:,36+i));
+legend('forceTotal','forceExcitation','forceRadiationDamping','forceAddedMass','forceRestoring','forceViscous','forceLinearDamping')
+xlabel('Time (s)')
+ylabel('Force (N) or Torque (N*m)')
+    title(['body1(barge) GBM dof ' int2str(i) ' Forces']);
+
