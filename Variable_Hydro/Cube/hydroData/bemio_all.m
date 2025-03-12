4a9ca15ea6f454ab5a5474682b0d75883b7f792f
akeeste <akeeste@sandia.gov>
2025-03-12 16:50:02 -0500

    add irf surface visualization to bemio

--- Variable_Hydro/Cube/hydroData/bemio_all.m
+++ Variable_Hydro/Cube/hydroData/bemio_all.m
@@ -21,7 +21,6 @@ for i = 1:length(angles)
     hydro.Vo = cubeVolume;
 
     hydro = radiationIRF(hydro, 30, [], [], [], []);
-    hydro = radiationIRFSS(hydro, [], []);
     hydro = excitationIRF(hydro, 30, [], [], [], []);
 
     hydro.plotDofs = [3 3];
@@ -37,17 +36,16 @@ for i = 1:length(angles)
 end
 
 % plot all BEM cases together
-nHydro = length(allHydro);
-tmp = mat2cell(allHydro, 1, ones(1,nHydro));
-plotBEMIO(tmp{:});
+% nHydro = length(allHydro);
+% tmp = mat2cell(allHydro, 1, ones(1,nHydro));
+% plotBEMIO(tmp{:});
 
 %% interpolate BEM data to greater resolution as the louvers open
 % Define hydro variables that need to be interpolated
-vars1D = {'fk_re', 'fk_im', 'fk_ma', 'fk_ph', ...
+vars = {'fk_re', 'fk_im', 'fk_ma', 'fk_ph', ...
           'sc_re', 'sc_im', 'sc_ma', 'sc_ph', ...
           'ex_re', 'ex_im', 'ex_ma', 'ex_ph', ...
-         };
-vars2D = {'A', 'Ainf', 'B'};
+          'A', 'Ainf', 'B', 'Khs', 'ra_K', 'ex_K'};
 
 % Define new angles
 newAngles = [0:5:90]; % new angle discretization
@@ -69,13 +67,9 @@ for i = nAngles0+1 : length(angles)
     allHydro(i).body = {refStr};
 
     dAngle = (angles(i) - angles(ind1)) / (angles(ind2) - angles(ind1)); % factor to linearly interpolate between louver angles
-    for iVar = 1:length(vars1D)
-        allHydro(i).(vars1D{iVar}) = allHydro(ind1).(vars1D{iVar}) * (1-dAngle) + ...
-                                     allHydro(ind2).(vars1D{iVar}) * dAngle;
-    end
-    for iVar = 1:length(vars2D)
-        allHydro(i).(vars2D{iVar}) = allHydro(ind1).(vars2D{iVar}) * (1-dAngle) + ...
-                                     allHydro(ind2).(vars2D{iVar}) * dAngle;
+    for iVar = 1:length(vars)
+        allHydro(i).(vars{iVar}) = allHydro(ind1).(vars{iVar}) * (1-dAngle) + ...
+                                     allHydro(ind2).(vars{iVar}) * dAngle;
     end
 end
 
@@ -84,9 +78,9 @@ end
 hydro_sorted = allHydro(iSorted);
 
 % plot all BEM and interpolated cases together
-% nHydro = length(allHydro);
-% tmp = mat2cell(allHydro, 1, ones(1,nHydro));
-% plotBEMIO(tmp{:});
+nHydro = length(hydro_sorted);
+tmp = mat2cell(hydro_sorted, 1, ones(1,nHydro));
+plotBEMIO(tmp{:});
 
 %% Write all data to h5 files
 for i = 1:length(hydro_sorted)
@@ -94,4 +88,22 @@ for i = 1:length(hydro_sorted)
     if ~isfile([hydro_sorted(i).file '.h5'])
         writeBEMIOH5(hydro_sorted(i));
     end
+end
+
+%% Visualize the IRF Surface
+radIrfSurf = nan(size(hydro_sorted(1).ra_K, 3), 6, 6, length(anglesSorted));
+for i = 1:length(hydro_sorted)
+    radIrfSurf(:,:,:,i) = permute(hydro_sorted(i).ra_K, [3 1 2])*1000;
+end
+
+dofTitle = {'Surge','Sway','Heave','Roll','Pitch','Yaw'};
+for dof = 3
+    radSurf = squeeze(radIrfSurf(:,dof,dof,:));
+    
+    figure()
+    contourf(anglesSorted, hydro_sorted(i).ra_t, radSurf);
+    xlabel('Flag angle (deg)');
+    ylabel('CIC Time (s); increasing backwards in time');
+    colorbar
+    title(['Radiation Impulse Response Function in ' dofTitle{dof} ' across the varying state']);
 end
